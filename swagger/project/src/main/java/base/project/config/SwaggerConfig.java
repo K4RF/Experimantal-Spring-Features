6c7825ee978f116f1d75216c0f00115ab0b0d434
KaarfSub <rpdirwk1025@naver.com>
2025-03-05 15:05:02 +0900

    [#1] feature: 스웨거 의존성 추가 및 빈 등록

---
+++ swagger/project/src/main/java/base/project/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package base.project.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi boardGroupedOpenApi() {
+        return GroupedOpenApi
+                .builder()
+                .group("base") // group 설정 (API들을 그룹화시켜 그룹에 속한 API들만 확인할 수 있도록 도와줌)
+                .pathsToMatch("/api/**") // group에 포함될 API endpoint 경로
+                .addOpenApiCustomizer(openApi ->
+                        openApi.setInfo(new Info()
+                                .title("base project api") // API 제목
+                                .description("추가 기능 개발을 위한 API") // API 설명
+                                .version("1.0.0") // API 버전
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        // SecurityScheme 설정 (JWT Bearer Token)
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .scheme("Bearer")
+                .bearerFormat("JWT");
+
+        // SecurityRequirement 설정 (Bearer Token 사용)
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        // OpenAPI 구성 반환
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey)) // SecurityScheme 등록
+                .addSecurityItem(securityRequirement) // SecurityRequirement 추가
+                .info(new Info()
+                        .title("Base Project api") // API 제목
+                        .description("추가 기능 개발을 위한 API") // API 설명
+                        .version("1.0.0") // API 버전
+                );
+    }
+}
