c32cd13819b9e2e63f746bb07ff5e751e631b9f1
KaarfSub <rpdirwk1025@naver.com>
2025-04-26 20:16:58 +0900

    [#9] features: h2 설정도 가능하도록 추가

--- jwt/social/src/main/java/jwt/project/controller/AuthController.java
+++ jwt/social/src/main/java/jwt/project/controller/AuthController.java
@@ -13,6 +13,7 @@ import jwt.project.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
