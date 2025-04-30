6c07a5d07770993eac4bbbd03650bb6d05efb89d
KaarfSub <rpdirwk1025@naver.com>
2025-04-30 22:20:54 +0900

    [#11] features: domain driven design 프로젝트 설정

---
+++ domain/src/test/java/base/project/DesignApplicationTests.java
@@ -0,0 +1,13 @@
+package base.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
