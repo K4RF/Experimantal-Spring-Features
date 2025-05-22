21732e6e4278f0addd6a3871668e0ffed0c84cf3
Kaarf <rpdirwk1025@naver.com>
2025-05-22 16:43:29 +0900

    [#15] features: 이메일 인증 관련 엔티티 추가

--- email/EmailVerification/src/main/java/jwt/project/entity/Member.java
+++ email/EmailVerification/src/main/java/jwt/project/entity/Member.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -32,4 +34,9 @@ public class Member {
     private SocialType socialType;
 
     private String socialId;      // 구글에서 제공하는 sub 값
+
+    // 이메일 인증 관련
+    private boolean emailVerified = false; // 이메일 인증 여부
+    private String emailVerificationToken; // 인증용 토큰
+    private LocalDateTime emailVerificationExpiry; // 만료 시간
 }
