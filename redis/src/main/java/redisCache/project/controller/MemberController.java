38c62a046cdf54a2eaecfdf9b97e842d0d5b1f3a
Kaarf <rpdirwk1025@naver.com>
2025-06-10 21:28:19 +0900

    [#19] features: DTO 활용해서 특정 정보만 response하도록 변경

--- redis/src/main/java/redisCache/project/controller/MemberController.java
+++ redis/src/main/java/redisCache/project/controller/MemberController.java
@@ -2,6 +2,7 @@ package redisCache.project.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import redisCache.project.dto.response.CacheResponse;
 import redisCache.project.entity.Member;
 import redisCache.project.service.MemberQueryService;
 
@@ -12,8 +13,14 @@ public class MemberController {
     private final MemberQueryService memberQueryService;
 
     @GetMapping("/{email}")
-    public Member getMember(@PathVariable String email) {
-        return memberQueryService.getMemberByEmail(email);
+    public CacheResponse getMember(@PathVariable String email) {
+        Member member = memberQueryService.getMemberByEmail(email);
+        return new CacheResponse(
+                member.getEmail(),
+                member.getName(),
+                member.getRole().name(),
+                member.isEmailVerified()
+        );
     }
 
     @DeleteMapping("/{email}/cache")
